e240931723d7bb555d883f953175b0947c1d9d6a
Williams Hernández <hernandw@gmail.com>
2023-10-19 12:59:56 -0300

    Updates

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+
+window.addEventListener("load", () => {
+    
+    try {
+      TagCanvas.Start("myCanvas", "tags", {
+        textColour: "#ff0000",
+        outlineColour: "#ff00ff",
+        reverse: true,
+        depth: 0.8,
+        maxSpeed: 0.05,
+      });
+    } catch (e) {
+      // something went wrong, hide the canvas container
+      document.getElementById("myCanvasContainer").style.display = "none";
+    }
+});
