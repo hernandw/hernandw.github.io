494be4d4020f578753ce9ce50a87949f6f3e9da6
Williams Hernández <hernandw@gmail.com>
2024-08-22 07:00:39 -0400

    :bug: error en deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className='flex justify-center items-center flex-col space-y-10'>
       <h1 className="text-3xl font-bold underline text-white text-center mt-20">Sitio Web en Mantenimiento</h1>
-      <p>Estamos Trabajando, Gracias</p>
+      <p className='text-center text-white'>Estamos Trabajando, Gracias</p>
       <img className='w-1/4'  src={images} alt="" />
 
     </div>
