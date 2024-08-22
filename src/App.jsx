78f18f0766953a22677cadbb6a06763c5d9fa9f2
Williams Hernández <hernandw@gmail.com>
2024-08-22 07:21:16 -0400

    :bug: prueba5 con deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     <div className='flex justify-center items-center flex-col space-y-10'>
       <h1 className="text-3xl font-bold underline text-white text-center mt-20">Sitio Web en Mantenimiento</h1>
       <p className='text-center text-white'>Estamos Trabajando, Gracias</p>
-      <p className='text-center text-white'>Pronto regresaremos</p>
+      
       <img className='w-1/4'  src={images} alt="" />
 
     </div>
