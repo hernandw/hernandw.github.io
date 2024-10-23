d909ccbd9f16fbc26ad0a7588eb5f93a443bd44e
Williams Hernández <hernandw@gmail.com>
2024-10-23 12:09:32 -0300

    :art: ajustes card del blog

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -9,7 +9,7 @@ const Blog = () => {
   return (
     <div className="w-full md:w-full h-full p-2 md:pl-28 md:pr-10  pt-16">
       <h1 className="text-4xl font-bold pb-10">Blog</h1>
-      <div className="flex flex-col md:flex-row flex-wrap justify-center md:justify-start items-center gap-10">
+      <div className="flex flex-col md:flex-row flex-wrap justify-center md:justify-start items-center gap-8">
       {blog.blog.map((item) => (
         <BlogCard key={item.id} {...item} />
       ))}
